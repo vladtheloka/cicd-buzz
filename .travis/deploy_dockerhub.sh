b2cd04683b9aa4b89699a318019c9c3dc1af8aed
vladtheloka <lokatheloka@gmail.com>
2019-04-07 22:46:14 +0300

    fixing

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS docker.io
+docker login -u vladtheloka1994 -p Vlad1994Loka docker.io
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
+docker push $TRAVIS_REPO_SLUG
