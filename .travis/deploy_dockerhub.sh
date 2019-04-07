bbf1726499ea313fb49729bfad6876e777e9d3fe
vladtheloka <lokatheloka@gmail.com>
2019-04-07 23:16:22 +0300

    final fixes

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-echo "$DOCKER_USER:$DOCKER_PASS"
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-echo $TRAVIS_REPO_SLUG:$TAG
-docker push $TRAVIS_REPO_SLUG:$TAG
+docker push $DOCKER_USER/cicd-buzz:$TAG
