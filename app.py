a197c93fb4c34640e1ab397a42edd01676e4a1c0
vladtheloka <lokatheloka@gmail.com>
2019-04-08 00:28:17 +0300

    sdfjs

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def generate_buzz():
     return page
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
+    app.run(host='0.0.0.0', port='5000')
